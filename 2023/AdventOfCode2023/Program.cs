d4b80bb49f8346f1f4df3f9c9d3e639d994bde9d
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-11-12 11:08:11 -0800

    Add main runner for 2023

--- 2023/AdventOfCode2023/Program.cs
+++ 2023/AdventOfCode2023/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class AdventOfCode2023
+{
+  public static void Main(string[] args)
+  {
+    if (args.Length != 1)
+    {
+      Console.WriteLine("Command needs exactly one argument specifying the day. e.g. dotnet run 1");
+      System.Environment.Exit(0);
+    }
+    int day = int.Parse(args[0]);
+
+    if (day > 25 || day < 1)
+    {
+      Console.WriteLine("Day must be between 1 and 25.");
+      System.Environment.Exit(0);
+    }
+
+    Console.WriteLine("Running day " + day);
+
+    switch (day)
+    {
+      case 1:
+        break;
+      default:
+        Console.WriteLine("Day not yet created/solved.");
+        break;
+    }
+  }
+}
+
