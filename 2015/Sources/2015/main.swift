c8fbc2cce196373a88511c1aa687d8a6c2b69107
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-11-18 21:20:09 -0800

    Adding time reporting for solutions

--- 2015/Sources/2015/main.swift
+++ 2015/Sources/2015/main.swift
@@ -1,8 +1,15 @@
-// Thanks @HarshilShah for this idea.
+import Foundation
 
+// Thanks @HarshilShah for this idea.
 typealias CurrentDay = Day01
+
+let startTime = Date()
 
 print("\n--- Input---")
 let day = CurrentDay(fileName: "Day01.txt")
 print("Part 1: \(day.Part01())")
 print("Part 2: \(day.Part02())")
+
+let endTime = Date()
+
+print("Run time (seconds): \(startTime.distance(to: endTime))")
