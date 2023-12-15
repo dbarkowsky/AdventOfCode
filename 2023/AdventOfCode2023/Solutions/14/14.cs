eba7e5b69a6aa1a2a14f52d5839ec33277b4fbb7
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-12-14 19:18:40 -0800

    2023 Day14 PArt 2 complete

--- 2023/AdventOfCode2023/Solutions/14/14.cs
+++ 2023/AdventOfCode2023/Solutions/14/14.cs
@@ -12,11 +12,11 @@ namespace Solutions
     public Day14(string fileName)
     {
       strings = FileReader.AsStringArray(fileName).ToList();
-      ResetGrid();
     }
 
     public int PartOne()
     {
+      ResetGrid();
       // Rocks need to go NORTH
       TiltNorth();
       // Sum the weight from each line
@@ -25,7 +25,14 @@ namespace Solutions
 
     public int PartTwo()
     {
-      return -1;
+      ResetGrid();
+      int iterations = 1000; // Seems to be enough... Don't need 1 billion
+      for (int i = 0; i < iterations; i++)
+      {
+        Cycle();
+      }
+
+      return GetLoad();
     }
 
     private void ResetGrid()
@@ -57,6 +64,14 @@ namespace Solutions
       return sum;
     }
 
+    private void Cycle()
+    {
+      TiltNorth();
+      TiltWest();
+      TiltSouth();
+      TiltEast();
+    }
+
     private void TiltNorth()
     {
       // Traverse grid each column from top to bottom
@@ -77,6 +92,85 @@ namespace Solutions
             if (currentX + 1 != rockPosition.x)
             {
               grid[currentX + 1][y] = 'O';
+              grid[rockPosition.x][rockPosition.y] = '.';
+            }
+          }
+        }
+      }
+    }
+
+    private void TiltSouth()
+    {
+      // Traverse grid each column from bottom to top
+      for (int y = 0; y < grid.First().Count; y++)
+      {
+        for (int x = grid.Count - 1; x >= 0; x--)
+        {
+          if (grid[x][y] == 'O')
+          {
+            // If we find an O rock, look down the column until we find another O, a #, or the edge
+            (int x, int y) rockPosition = (x, y);
+            int currentX = x + 1;
+            while (currentX < grid.Count && grid[currentX][y] != 'O' && grid[currentX][y] != '#')
+            {
+              currentX++;
+            }
+            // Then swap the current rock with the last known free position
+            if (currentX - 1 != rockPosition.x)
+            {
+              grid[currentX - 1][y] = 'O';
+              grid[rockPosition.x][rockPosition.y] = '.';
+            }
+          }
+        }
+      }
+    }
+
+    private void TiltWest()
+    {
+      for (int x = 0; x < grid.Count; x++)
+      {
+        for (int y = 0; y < grid.First().Count; y++)
+        {
+          if (grid[x][y] == 'O')
+          {
+            // If we find an O rock, look down the column until we find another O, a #, or the edge
+            (int x, int y) rockPosition = (x, y);
+            int currentY = y - 1;
+            while (currentY >= 0 && grid[x][currentY] != 'O' && grid[x][currentY] != '#')
+            {
+              currentY--;
+            }
+            // Then swap the current rock with the last known free position
+            if (currentY + 1 != rockPosition.y)
+            {
+              grid[x][currentY + 1] = 'O';
+              grid[rockPosition.x][rockPosition.y] = '.';
+            }
+          }
+        }
+      }
+    }
+
+    private void TiltEast()
+    {
+      for (int x = 0; x < grid.Count; x++)
+      {
+        for (int y = grid.First().Count - 1; y >= 0; y--)
+        {
+          if (grid[x][y] == 'O')
+          {
+            // If we find an O rock, look down the column until we find another O, a #, or the edge
+            (int x, int y) rockPosition = (x, y);
+            int currentY = y + 1;
+            while (currentY < grid.First().Count && grid[x][currentY] != 'O' && grid[x][currentY] != '#')
+            {
+              currentY++;
+            }
+            // Then swap the current rock with the last known free position
+            if (currentY - 1 != rockPosition.y)
+            {
+              grid[x][currentY - 1] = 'O';
               grid[rockPosition.x][rockPosition.y] = '.';
             }
           }
