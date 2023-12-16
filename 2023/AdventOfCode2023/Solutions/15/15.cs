64c6f0c52d6b6f6efffabdff86a7793900159e8f
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-12-15 18:19:18 -0800

    2023 Day15 Part2 Complete

--- 2023/AdventOfCode2023/Solutions/15/15.cs
+++ 2023/AdventOfCode2023/Solutions/15/15.cs
@@ -17,14 +17,7 @@ namespace Solutions
       int sum = 0;
       foreach (string code in strings)
       {
-        int currentValue = 0;
-        foreach (char character in code.ToCharArray())
-        {
-          currentValue += GetASCII(character);
-          currentValue *= 17;
-          currentValue %= 256;
-        }
-        sum += currentValue;
+        sum += GetHash(code);
       }
       return sum;
     }
@@ -33,9 +26,82 @@ namespace Solutions
     {
       // Can definitely do this with a hash table or dictionary, each slot containing a list
       // Maybe even easier with an array of lists
+      List<Lens>[] boxes = new List<Lens>[256];
+      for (int i = 0; i < boxes.Length; i++)
+      {
+        boxes[i] = new List<Lens>();
+      }
       // The modulus result is the hash. -> would be index in array
       // Insert values into the appropriate list where needed
-      return -1;
+      foreach (string code in strings)
+      {
+        // Is it an assignment or subtraction?
+        bool isAssignment = code.Contains('=');
+        string hashInput;
+        if (isAssignment)
+        {
+          hashInput = code.Split("=").First();
+        }
+        else
+        {
+          hashInput = code.Split("-").First();
+        }
+        // Get hash
+        int hash = GetHash(hashInput);
+        if (isAssignment)
+        {
+          int codeValue = int.Parse(code.Split("=").Last());
+          int index = boxes[hash].FindIndex((Lens item) => item.label == hashInput);
+          // Returns -1 if not found
+          if (index >= 0)
+          {
+            // Replace with new lens value
+            boxes[hash][index].value = codeValue;
+          }
+          else
+          {
+            // Add to the end of the lens list
+            boxes[hash].Add(new Lens(hashInput, codeValue));
+          };
+        }
+        else
+        {
+          boxes[hash].RemoveAll((Lens item) => item.label == hashInput);
+        }
+      }
+      // Calculate the focusing power
+      int sum = 0;
+      foreach ((List<Lens> lenses, int i) in boxes.WithIndex())
+      {
+        foreach ((Lens lens, int j) in lenses.WithIndex())
+        {
+          sum += (i + 1) * (j + 1) * lens.value;
+        }
+      }
+      return sum;
+    }
+
+    class Lens
+    {
+      public string label;
+      public int value;
+      public Lens(string label, int value)
+      {
+        this.label = label;
+        this.value = value;
+      }
+    }
+
+    private int GetHash(string code)
+    {
+      int currentValue = 0;
+      foreach (char character in code.ToCharArray())
+      {
+        currentValue += GetASCII(character);
+        currentValue *= 17;
+        currentValue %= 256;
+      }
+      return currentValue;
     }
 
     private int GetASCII(char character)
