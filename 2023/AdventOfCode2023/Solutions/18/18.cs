4a59db39b07b644487e13bf9dd824000745db011
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-12-20 23:44:18 -0800

    2023 Day18 Part2 Complete

--- 2023/AdventOfCode2023/Solutions/18/18.cs
+++ 2023/AdventOfCode2023/Solutions/18/18.cs
@@ -16,12 +16,37 @@ namespace Solutions
 
     public double PartOne()
     {
+      List<(int x, int y)> pathList = FollowPath(instructions);
+      return GetArea(pathList);
+    }
+
+    public long PartTwo()
+    {
+      // Convert hex to new instructions
+      List<(string direction, int distance, string colour)> instructionsPart2 = instructions.Select(instruction =>
+      {
+        // This is a cool way to do a switch on a string (returned by the substring)
+        string direction = instruction.colour.Substring(6, 1) switch
+        {
+          "0" => "R",
+          "1" => "D",
+          "2" => "L",
+          "3" => "U",
+          _ => "." // Should never reach that
+        };
+        int distance = Convert.ToInt32($"{instruction.colour.Substring(1, 5)}", 16);
+        return (direction, distance, instruction.colour);
+      }).ToList();
+      List<(int x, int y)> pathList = FollowPath(instructionsPart2);
+      return GetArea(pathList);
+    }
+
+    private List<(int x, int y)> FollowPath(List<(string direction, int distance, string colour)> instructions)
+    {
       int currentX = 0;
       int currentY = 0;
       // Set to store all locations visited
       HashSet<(int x, int y)> path = new();
-
-      int area = 0;
 
       // Loop through all instructions, adding coordinates to set
       foreach ((string direction, int distance, string colour) instruction in instructions)
@@ -49,28 +74,26 @@ namespace Solutions
           path.Add((currentX, currentY));
         }
       }
+      return path.ToList();
+    }
 
+    private long GetArea(List<(int x, int y)> path)
+    {
       // shoelace algorithm
       // I was doing something wrong here for a long time. Was always like 2 off
       // This for-loop solution from Ryan Heath
-      List<(int x, int y)> pathList = path.ToList();
-      for (int i = 0; i < pathList.Count; i++)
+      long area = 0;
+      for (int i = 0; i < path.Count; i++)
       {
-        int nextI = (i + 1) % pathList.Count;
-        int prevI = i - 1 < 0 ? pathList.Count - 1 : i - 1;
-        area += pathList[i].y * (pathList[nextI].x - pathList[prevI].x);
+        int nextI = (i + 1) % path.Count;
+        int prevI = i - 1 < 0 ? path.Count - 1 : i - 1;
+        area += path[i].y * (path[nextI].x - path[prevI].x);
       }
 
       area = Math.Abs(area) / 2;
       // Need to add the perimeter to the area
-      area += pathList.Count / 2 + 1;
-
+      area += path.Count / 2 + 1;
       return area;
-    }
-
-    public int PartTwo()
-    {
-      return -1;
     }
 
     private List<(string direction, int distance, string colour)> ParseInput(List<string> strings)
