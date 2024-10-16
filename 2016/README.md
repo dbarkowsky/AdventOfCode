0be562addd69b19c637460ff710884223a4b29fb
dbarkowsky <dylanbarkowsky@gmail.com>
2024-10-16 10:12:01 -0700

    command line args for rust

--- 2016/README.md
+++ 2016/README.md
@@ -2,4 +2,6 @@
 
 This year is written in Rust.
 
-From the `/aoc2016` folder, use the command `cargo run` to run the solution.
+From the `/aoc2016` folder, use the command `cargo run <day number>` to run the solution.
+
+For example, `cargo run 1` will run Day 01.
--- 2016/aoc2016/src/main.rs
+++ 2016/aoc2016/src/main.rs
@@ -1,7 +1,15 @@
 mod days;
 mod utils;
+use std::env;
 
 fn main() {
-    days::day_01::part_one();
-    days::day_01::part_two();
+    let args: Vec<String> = env::args().collect();
+    let day: i32 = args[1].parse().unwrap();
+    match day {
+        1 => {
+            days::day_01::part_one();
+            days::day_01::part_two();
+        }
+        i32::MIN..=0_i32 | 2_i32..=i32::MAX => todo!(),
+    }
 }
