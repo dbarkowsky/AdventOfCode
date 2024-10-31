33c96b77ba3e3ccd984215461eb921409fe59675
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2024-10-30 22:02:13 -0700

    2016 day 7 part 2

--- 2016/aoc2016/src/days/day_07.rs
+++ 2016/aoc2016/src/days/day_07.rs
@@ -1,19 +1,19 @@
-use std::{cmp::Ordering, collections::HashMap};
+use std::collections::HashMap;
 use crate::utils;
 
 const DAY: &str = "07";
 const TEST: bool = false; 
+// Started using references on this one. Need to keep this in mind going forward.
+// Made things easier. Need to remember my C++.
 
 // Part 1 answer for test data = 2
+// How many have abba outside but no abba inside?
 pub fn part_one() {
     let input: Vec<HashMap<String, Vec<String>>> = parse_input();
-    let mut tls_count = 0;
+    let mut tls_count: i32 = 0;
     for line in input {
-      let inside_abba_count = count_abba_in_list(line.get("inside").unwrap().to_vec());
-      let outside_abba_count = count_abba_in_list(line.get("outside").unwrap().to_vec());
-      // println!("{:#?}", line);
-      // println!("{:#?}", outside_abba_count);
-      // println!("{:#?}", inside_abba_count);
+      let inside_abba_count: i32 = count_abba_in_list(line.get("inside").unwrap().to_vec());
+      let outside_abba_count: i32 = count_abba_in_list(line.get("outside").unwrap().to_vec());
 
       if outside_abba_count >= 1 && inside_abba_count == 0 {
         tls_count += 1;
@@ -22,11 +22,57 @@ pub fn part_one() {
     println!("{}", tls_count);
 }
 
+// Part 2 answer for test data = 3
+// How many have bab inside when having aba outside?
 pub fn part_two() {
+  let input: Vec<HashMap<String, Vec<String>>> = parse_input();
+    let mut ssl_count: i32 = 0;
+    for line in input {
+      let aba_list: Vec<String> = get_abas_in_list(line.get("outside").unwrap().to_vec());
+      if has_reverse_aba(&aba_list, &line.get("inside").unwrap().to_vec()) {
+        ssl_count += 1;
+      }
+    }
+    println!("{}", ssl_count);
 }
 
+// Does a list have any of the reversed abas found in other list?
+// Uses references to avoid ownership issues
+fn has_reverse_aba(list_of_abas: &Vec<String>, list_to_check_against: &Vec<String>) -> bool {
+  for aba in list_of_abas {
+    let reverse_aba: String = reverse_aba(&aba);
+    for list_segment in list_to_check_against {
+      if list_segment.contains(&reverse_aba.as_str()) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// Just reverses a 3-character string.
+fn reverse_aba(aba: &String) -> String {
+  let letters: Vec<char> = aba.chars().into_iter().collect();
+  return String::from_iter([letters[1], letters[0], letters[1]].iter()) 
+}
+
+// Returns all 3-character palindromes in a list of strings
+fn get_abas_in_list(list: Vec<String>) -> Vec<String> {
+  let mut aba_list: Vec<String> = Vec::new();
+  for segment in list {
+    let letters: Vec<char> = segment.chars().into_iter().collect();
+    for i in 0..(segment.len() - 2) {
+      if letters[i] == letters[i + 2] && letters[i] != letters[i+1]{
+        aba_list.push([letters[i], letters[i + 1], letters[i + 2]].iter().collect());
+      }
+    }
+  }
+  return aba_list;
+}
+
+// Gets counts of 4-character palindromes
 fn count_abba_in_list(list: Vec<String>) -> i32 {
-  let mut count = 0;
+  let mut count: i32 = 0;
   for element in list {
     // Check if it contains a palindrome
     if has_palindrome(element){
@@ -36,9 +82,10 @@ fn count_abba_in_list(list: Vec<String>) -> i32 {
   return count;
 }
 
+// We're only interested in 4-character long palindromes
+// Outside and inside letters must be different
 fn has_palindrome(segment: String) -> bool {
   let letters: Vec<char> = segment.chars().into_iter().collect();
-  // println!("{:#?}", letters);
   for i in 0..(segment.len() - 3) {
     if letters[i] == letters[i + 3] && letters[i + 1] == letters[i + 2] && letters[i] != letters[i+1]{
       return true;
@@ -48,15 +95,18 @@ fn has_palindrome(segment: String) -> bool {
 }
 
 fn parse_input()  -> Vec<HashMap<String, Vec<String>>>{
-    let test_suffix = if TEST { ".test" } else { "" };
+    let test_suffix: &str = if TEST { ".test" } else { "" };
     let file_path: String = format!("./input/day{day}{test}.txt", day = DAY, test = test_suffix);
     let input: Vec<String> = utils::file_reader::file_to_line_array(&file_path);
+    // Convert these lines to a hash map list with lists of outside and inside strings
     let mut separated_input: Vec<HashMap<String, Vec<String>>> = Vec::new();
     for line in input {
+      // Split on [ or ]
       let segments: Vec<&str> = line.split(|character| character == '[' || character == ']').collect();
-      let mut outside_segments = Vec::new();
-      let mut inside_segments = Vec::new();
-      let mut line_map = HashMap::new();
+      let mut outside_segments: Vec<String> = Vec::new();
+      let mut inside_segments: Vec<String> = Vec::new();
+      let mut line_map: HashMap<String, Vec<String>> = HashMap::new();
+      // All even segments are outside, all odd are inside
       for i in 0..segments.len() {
         if i % 2 == 0 {
           outside_segments.push(String::from(segments[i]));
