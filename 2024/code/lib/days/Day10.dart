01ba7b84d1a660cc1ae5bbad1f7adf8b1b52ea6d
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2024-12-10 07:53:31 -0800

    2024 day10 part 2

--- 2024/code/lib/days/Day10.dart
+++ 2024/code/lib/days/Day10.dart
@@ -6,21 +6,16 @@ class Day10 extends Day {
     map = input.map((row) => row.split("").map(int.parse).toList()).toList();
   }
 
+  // Counting unique endpoints for each trailhead
   void part1() {
     List<(int, int)> zeros = findAllZeros(map);
 
     int count = 0;
 
     for (final zero in zeros) {
+      // Using Set so that each peak is only counted once, even if multiple paths
       Set<String> uniquePeaks = {};
-      List<List<bool>> visited = [];
-      for (int i = 0; i < map.length; i++) {
-        List<bool> row = [];
-        for (int j = 0; j < map[i].length; j++) {
-          row.add(false);
-        }
-        visited.add(row);
-      }
+      List<List<bool>> visited = makeVisited();
       List<(int, int)> peaks = countPathsFrom(zero, visited);
       for (final peak in peaks) {
         uniquePeaks.add(peak.toString());
@@ -31,21 +26,16 @@ class Day10 extends Day {
     print(count);
   }
 
+  // Counting all possible routes from trailhead (0) to end (9)
   void part2() {
     List<(int, int)> zeros = findAllZeros(map);
 
     int count = 0;
 
     for (final zero in zeros) {
-      List<String> pathEnds = [];
-      List<List<bool>> visited = [];
-      for (int i = 0; i < map.length; i++) {
-        List<bool> row = [];
-        for (int j = 0; j < map[i].length; j++) {
-          row.add(false);
-        }
-        visited.add(row);
-      }
+      List<String> pathEnds =
+          []; // This is the key difference. We count all possible routes
+      List<List<bool>> visited = makeVisited();
       List<(int, int)> peaks = countPathsFrom(zero, visited);
       for (final peak in peaks) {
         pathEnds.add(peak.toString());
@@ -56,11 +46,27 @@ class Day10 extends Day {
     print(count);
   }
 
+  // Makes grid of visited locations
+  List<List<bool>> makeVisited() {
+    List<List<bool>> visited = [];
+    for (int i = 0; i < map.length; i++) {
+      List<bool> row = [];
+      for (int j = 0; j < map[i].length; j++) {
+        row.add(false);
+      }
+      visited.add(row);
+    }
+    return visited;
+  }
+
+  // Recursive function to do a Breadth-first search
   List<(int, int)> countPathsFrom((int, int) start, List<List<bool>> visited,
       {int increment = 1, int endValue = 9, (int, int)? origin}) {
+    // Need to copy list like this to make actual deep copy
+    List<List<bool>> localVisited = List.from(visited.map((row) => [...row]));
+    // If it's the end value, we're found what we're looking for.
     if (map[start.$1][start.$2] == endValue) return [start];
-    List<List<bool>> localVisited = List.from(visited);
-    printVisited(localVisited);
+    // Mark as visited
     localVisited[start.$1][start.$2] = true;
     List<(int, int)> nextLocations = [];
     // Add locations on all sides
@@ -75,18 +81,15 @@ class Day10 extends Day {
       // It can't have been visited
       if (localVisited[location.$1][location.$2]) continue;
       // It can't be a different difference than the increment
-      if (map[location.$1][location.$2] - map[start.$1][start.$2] != increment)
+      if (map[location.$1][location.$2] - map[start.$1][start.$2] !=
+          increment) {
         continue;
-      // If an origin was specified, it can't be from there.
-      if (origin != null &&
-          location.$1 == origin.$1 &&
-          location.$2 == origin.$2) continue;
-
+      }
       // Otherwise, add
       nextLocations.add(location);
-      // print('$start $location');
     }
     if (nextLocations.isEmpty) return [];
+    // Concatenate all found paths together
     List<(int, int)> foundPaths = [];
     for (final next in nextLocations) {
       foundPaths.addAll(countPathsFrom(next, localVisited, origin: start));
@@ -94,6 +97,7 @@ class Day10 extends Day {
     return foundPaths;
   }
 
+  // Find and list all coodinates with 0
   List<(int, int)> findAllZeros(List<List<int>> map) {
     List<(int, int)> zeros = [];
     for (int x = 0; x < map.length; x++) {
@@ -104,8 +108,9 @@ class Day10 extends Day {
     return zeros;
   }
 
-  void printVisited(List<List<bool>> visited){
-    for (final row in visited){
+  // For testing purposes
+  void printVisited(List<List<bool>> visited) {
+    for (final row in visited) {
       print(row.map((el) => el ? "X" : ".").join(""));
     }
     print("");
