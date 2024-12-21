27bc9d4feda2b6c932b16d34de26844551f1de52
Kiereek <dylanbarkowsky@gmail.com>
2024-12-21 15:48:14 -0800

    2024 day 21 part 1

--- 2024/code/lib/days/Day21.dart
+++ 2024/code/lib/days/Day21.dart
@@ -1,8 +1,15 @@
+import 'dart:collection';
 import 'dart:core';
 import 'package:code/days/Day.dart';
 
 // My good friend, Coordinate, back again.
 typedef Coordinate = ({int x, int y});
+
+class SearchState {
+  Coordinate location;
+  List<Coordinate> visited = [];
+  SearchState(this.location, this.visited);
+}
 
 class Day21 extends Day {
   Day21(super.fileName, super.useTestData);
@@ -19,7 +26,6 @@ class Day21 extends Day {
     '3': (x: 2, y: 2),
     '0': (x: 3, y: 1),
     'A': (x: 3, y: 2),
-    'C': (x: 1, y: 1),
   };
 
   Map<String, Coordinate> dirPad = {
@@ -28,103 +34,161 @@ class Day21 extends Day {
     '<': (x: 1, y: 0),
     'v': (x: 1, y: 1),
     '>': (x: 1, y: 2),
-    'C': (x: 1, y: 1),
   };
-  // NOTE: If you're moving towards the "centre" (C), move horizontal first. If moving away from "centre", move vertically first.
+
+  Map<(String, String), String> cache = {};
 
   void part1() {
     int total = 0;
-    String code = '379A'; // 24256
-    // for (final code in input){
-      // Get initial directions for numPad
-      List<String> numPadDirections = convertToNext(code.split(""), numPad);
-      // Convert for middle robot
-      List<String> middleDirections = convertToNext(numPadDirections, dirPad);
-      // Convert again for human (me)
-      List<String> humanDirections = convertToNext(middleDirections, dirPad);
+    for (final code in input) {
+      String start = 'A';
+      String joinedDirections = "";
+      for (int i = 0; i < code.length; i++) {
+        String end = code[i];
+        // Get initial inputs for possible keypad presses
+        List<List<Coordinate>> paths = getShortestPaths(start, end, numPad);
+        // Convert to direction strings
+        List<List<String>> directions =
+            paths.map(convertPathToInstructions).toList();
+        // Use converter to get smallest amount of steps at x depth of robots
+        String convertedDirections = layerRobots(2, directions);
+        joinedDirections += convertedDirections;
+        start = code[i];
+      }
       // Get complexity score
       RegExp number = RegExp(r"[\d]+");
       int numericPart = int.parse(number.firstMatch(code)!.group(0)!);
-      total += humanDirections.length * numericPart;
-      /// <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
-      /// v<<A>>^A<A>AvA<^AA>A<vAAA>^A
-      /// <A^A>^^AvvvA
-      /// 029A
-      print(humanDirections.join(""));
-      print(middleDirections.join(""));;
-      print(numPadDirections.join(""));
-      print(code);
-      print(humanDirections.length * numericPart);
-    // }
+      total += joinedDirections.length * numericPart;
+    }
     print(total);
   }
 
   void part2() {
-  }
-
-  // FIXME: Could cache these results if we tracked depth of robots + pad
-  List<String> convertToNext(List<String> input, Map<String, Coordinate> pad){
-    List<String> moves = [];
-    String current = 'A';
-    for (final piece in input){
-      // Get moves from current to this piece
-      List<String> movesToPiece = getMovesBetween(current, piece, pad);
-      // Add moves to list
-      moves.addAll(movesToPiece);
-      moves.add('A');
-      // Update current
-      current = piece;
+    int total = 0;
+    for (final code in input) {
+      String start = 'A';
+      String joinedDirections = "";
+      for (int i = 0; i < code.length; i++) {
+        String end = code[i];
+        // Get initial inputs for possible keypad presses
+        List<List<Coordinate>> paths = getShortestPaths(start, end, numPad);
+        // Convert to direction strings
+        List<List<String>> directions =
+            paths.map(convertPathToInstructions).toList();
+        // Use converter to get smallest amount of steps at x depth of robots
+        String convertedDirections = layerRobots(25, directions);
+        joinedDirections += convertedDirections;
+        start = code[i];
+      }
+      // Get complexity score
+      RegExp number = RegExp(r"[\d]+");
+      int numericPart = int.parse(number.firstMatch(code)!.group(0)!);
+      total += joinedDirections.length * numericPart;
     }
-    return moves;
+    print(total);
   }
 
-  // FIXME: There's something here where we need to consider multiple different possible moves. 
-  List<String> getMovesBetween(String start, String end, Map<String, Coordinate> pad){
-    List<String> moves = [];
-    int verticalDiff = pad[end]!.x - pad[start]!.x;
-    int horizontalDiff = pad[end]!.y - pad[start]!.y;
-    // If we stay on the same button, return empty moves.
-    if (start == end) return moves;
-    bool doHorizontalFirst = pad == numPad ? verticalDiff > 0 ? true : false : verticalDiff > 0 ? false : true;
-    // Are we going away from centre or towards it?
-    // This decision helps avoid the blank space on both pads
-    if (doHorizontalFirst){
-      // We are getting closer, do horizontal first
-      if (horizontalDiff > 0){
-        // We're going right
-        moves.addAll(List.filled(horizontalDiff, '>'));
-      } else if (horizontalDiff < 0){
-        // We're going left
-        moves.addAll(List.filled(horizontalDiff.abs(), '<'));
+  String layerRobots(int remainingLayers, List<List<String>> directions) {
+    if (directions.isEmpty) return "";
+    if (remainingLayers == 0) {
+      return directions.first.join("");
+    }
+    List<List<String>> newDirections = [];
+    for (final possibleDirections in directions) {
+      if (possibleDirections.isEmpty) continue;
+      String start = 'A';
+      String joinedDirections = "";
+      for (int i = 0; i < possibleDirections.length; i++) {
+        String end = possibleDirections[i];
+        // Get initial inputs for possible keypad presses
+        List<List<Coordinate>> paths = getShortestPaths(start, end, dirPad);
+        // Convert to direction strings
+        List<List<String>> directions =
+            paths.map(convertPathToInstructions).toList();
+        // print('$start, $end, ${directions.join("")}');
+        // Use converter to get smallest amount of steps at x depth of robots
+        String convertedDirections =
+            layerRobots(remainingLayers - 1, directions);
+        joinedDirections += convertedDirections;
+        start = possibleDirections[i];
       }
-      if (verticalDiff > 0){
-        // We're going down
-        moves.addAll(List.filled(verticalDiff, 'v'));
-      } else if (verticalDiff < 0){
-        // We're going up
-        moves.addAll(List.filled(verticalDiff.abs(), '^'));
-      }
-    } else {
-      // We are getting farther, do vertical first
-      if (verticalDiff > 0){
-        // We're going down
-        moves.addAll(List.filled(verticalDiff, 'v'));
-      } else if (verticalDiff < 0){
-        // We're going up
-        moves.addAll(List.filled(verticalDiff.abs(), '^'));
-      }
-      if (horizontalDiff > 0){
-        // We're going right
-        moves.addAll(List.filled(horizontalDiff, '>'));
-      } else if (horizontalDiff < 0){
-        // We're going left
-        moves.addAll(List.filled(horizontalDiff.abs(), '<'));
+      newDirections.add(joinedDirections.split("").toList());
+    }
+    // All sublists have been converted, find the shortest one.
+    List<String> stringDirs = newDirections.map((e) => e.join("")).toList();
+    stringDirs.sort((a, b) => a.length - b.length);
+    return stringDirs.first;
+  }
+
+  List<String> convertPathToInstructions(List<Coordinate> path) {
+    List<String> instructions = [];
+    for (int i = 1; i < path.length; i++) {
+      Coordinate a = path[i - 1];
+      Coordinate b = path[i];
+
+      if (b.x > a.x) {
+        // moving down
+        instructions.add("v");
+      } else if (a.x > b.x) {
+        // moving up
+        instructions.add("^");
+      } else if (b.y > a.y) {
+        // moving right
+        instructions.add(">");
+      } else if (a.y > b.y) {
+        // moving left
+        instructions.add("<");
       }
     }
-    return moves;
+    instructions.add('A');
+    return instructions;
   }
 
-  int getDistance(Coordinate a, Coordinate b){
-    return (a.x - b.x).abs() + (a.y - b.y).abs();
+  // TODO: Cache this too
+  List<List<Coordinate>> getShortestPaths(
+      String from, String to, Map<String, Coordinate> pad) {
+    List<List<Coordinate>> paths = [];
+    Coordinate start = pad[from]!;
+    Coordinate end = pad[to]!;
+    Queue<SearchState> queue = Queue();
+    queue.add(SearchState(start, [start]));
+    while (queue.isNotEmpty) {
+      SearchState current = queue.removeFirst();
+      if (current.location == end) {
+        paths.add(current.visited);
+        continue;
+      }
+
+      List<Coordinate> neighbours =
+          getNextNeighbours(current.location, 3, current.visited)
+              .where((n) => pad.containsValue(n))
+              .toList();
+      queue.addAll(
+          neighbours.map((n) => SearchState(n, [...current.visited, n])));
+    }
+    int min = paths.first.length;
+    for (final path in paths) {
+      if (path.length < min) {
+        min = path.length;
+      }
+    }
+    return paths.where((p) => p.length == min).toList();
+  }
+
+  List<Coordinate> getNextNeighbours(
+      Coordinate start, int max, List<Coordinate> exclude,
+      {int withinDistance = 1}) {
+    List<Coordinate> offsets = [];
+    for (int x = -withinDistance; x <= withinDistance; x++) {
+      for (int y = -withinDistance; y <= withinDistance; y++) {
+        if (x.abs() + y.abs() <= withinDistance) offsets.add((x: x, y: y));
+      }
+    }
+    return offsets
+        .map((offset) => (x: start.x + offset.x, y: start.y + offset.y))
+        .where((coord) =>
+            coord.x >= 0 && coord.y >= 0 && coord.x <= max && coord.y <= max)
+        .where((coord) => !exclude.contains(coord))
+        .toList();
   }
 }
