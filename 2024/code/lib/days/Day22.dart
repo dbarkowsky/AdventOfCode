a75612d49ccdd77d75a374476d10f06bda13b9ca
Kiereek <dylanbarkowsky@gmail.com>
2024-12-22 22:20:51 -0700

    2024 day 22 part 2

--- 2024/code/lib/days/Day22.dart
+++ 2024/code/lib/days/Day22.dart
@@ -5,10 +5,10 @@ class Day22 extends Day {
 
   void part1() {
     int sum = 0;
-    for (final secretString in input){
+    for (final secretString in input) {
       int current = int.parse(secretString);
       int goal = 2000;
-      for (int i = 0; i < goal; i++){
+      for (int i = 0; i < goal; i++) {
         current = prune(mix(current, current * 64));
         current = prune(mix(current, (current / 32).floor()));
         current = prune(mix(current, current * 2048));
@@ -19,14 +19,45 @@ class Day22 extends Day {
   }
 
   void part2() {
-    
+    Map<String, int> priceSums = {};
+    for (final secretString in input) {
+      int current = int.parse(secretString);
+      int goal = 2000;
+      List<int> prices = [current % 10];
+      List<int> priceDiffs = [];
+      for (int i = 0; i < goal - 1; i++) {
+        current = prune(mix(current, current * 64));
+        current = prune(mix(current, (current / 32).floor()));
+        current = prune(mix(current, current * 2048));
+        prices.add(current % 10);
+        priceDiffs.add(prices.last - prices[prices.length - 2]);
+      }
+      Map<String, int> highestPrices = {};
+      int priceIndex = 4; // First time that we have four consecutive changes
+      for (int diffIndex = 3; diffIndex < priceDiffs.length; diffIndex++) {
+        String key = priceDiffs.sublist(diffIndex - 3, diffIndex + 1).join(",");
+        highestPrices.update(key, (value) => value,
+            ifAbsent: () => prices[priceIndex]);
+        priceIndex++;
+      }
+      // Add these new totals to the price tracker
+      for (final entry in highestPrices.entries) {
+        priceSums.update(entry.key, (value) => value + entry.value,
+            ifAbsent: () => entry.value);
+      }
+    }
+    // Identify combo with highest sum
+    List<(String, int)> highestPricesList =
+        priceSums.entries.map((el) => (el.key, el.value)).toList();
+    highestPricesList.sort((a, b) => b.$2 - a.$2);
+    print(highestPricesList.first);
   }
 
-  int mix(int secret, int value){
-    return value^secret;
+  int mix(int secret, int value) {
+    return value ^ secret;
   }
 
-  int prune(int secret){
+  int prune(int secret) {
     return secret % 16777216;
   }
 }
