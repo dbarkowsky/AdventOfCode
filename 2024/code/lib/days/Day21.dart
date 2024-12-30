5056107a8f32a637d476bd58451f2dae851d86ff
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2024-12-29 16:27:25 -0800

    2024 day 21 part 2

--- 2024/code/lib/days/Day21.dart
+++ 2024/code/lib/days/Day21.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 import 'dart:core';
+import 'dart:math';
 import 'package:code/days/Day.dart';
 
 // My good friend, Coordinate, back again.
@@ -14,6 +15,7 @@ class SearchState {
 class Day21 extends Day {
   Day21(super.fileName, super.useTestData);
 
+  // Some hardcoded coordinates for the two different pads
   Map<String, Coordinate> numPad = {
     '7': (x: 0, y: 0),
     '8': (x: 0, y: 1),
@@ -35,133 +37,120 @@ class Day21 extends Day {
     'v': (x: 1, y: 1),
     '>': (x: 1, y: 2),
   };
-  Map<String, String> cache = {};
+
+  // This cache remembers what the final length of an input will be
+  // key is "directions,depth"  e.g. "<<A,3"
+  Map<String, int> cache = {};
   void part1() {
-    int total = 0;
-    for (final code in input) {
-      String start = 'A';
-      String joinedDirections = "";
-      for (int i = 0; i < code.length; i++) {
-        String end = code[i];
-        // Get initial inputs for possible keypad presses
-        List<List<Coordinate>> paths = getShortestPaths(start, end, numPad);
-        // Convert to direction strings
-        List<List<String>> directions =
-            paths.map(convertPathToInstructions).toList();
-        // Use converter to get smallest amount of steps at x depth of robots
-        String convertedDirections = layerRobots(2, directions);
-        joinedDirections += convertedDirections;
-        start = code[i];
-      }
-      // Get complexity score
-      RegExp number = RegExp(r"[\d]+");
-      int numericPart = int.parse(number.firstMatch(code)!.group(0)!);
-      total += joinedDirections.length * numericPart;
-    }
-    print(total);
+    print(solve(2));
   }
 
   void part2() {
+    print(solve(25));
+  }
+
+  int solve(int steps) {
+    // Reset cache, just in case
+    cache = {};
     int total = 0;
-    int steps = 25;
+    // For each input code
     for (final code in input) {
-      print(code);
+      // We always start at A
       String start = 'A';
-      String joinedDirections = "";
+      int directionLength = 0;
+      // Progress along the code
       for (int i = 0; i < code.length; i++) {
         String end = code[i];
-        // TODO: if we've seen this start and end before, we should know the end result to return
         // Get initial inputs for possible keypad presses
         List<List<Coordinate>> paths = getShortestPaths(start, end, numPad);
         // Convert to direction strings
         List<List<String>> directions = paths
             .map(convertPathToInstructions)
-            .where(hasNoInefficiencies)
+            .where(hasNoInefficiencies) // Removes things like <,^,<
             .toList();
-        // print(directions);
-        String convertedDirections = "";
-
         // Use converter to get smallest amount of steps at x depth of robots
-        convertedDirections = layerRobots(steps, directions);
-
-        joinedDirections += convertedDirections;
+        // Have to check all possible outcomes, because it's not always clear which one is best at any given depth
+        directionLength +=
+            directions.map((d) => layerRobots(steps, d.join(""))).reduce(min);
+        // Move to next symbol
         start = code[i];
       }
       // Get complexity score
       RegExp number = RegExp(r"[\d]+");
       int numericPart = int.parse(number.firstMatch(code)!.group(0)!);
-      total += joinedDirections.length * numericPart;
+      total += directionLength * numericPart;
     }
-    print(total);
+    return total;
   }
 
+  // Checks if a direction list has things like <,^.< (bad)
+  // Should only keep things like <,<,^ instead (good)
   bool hasNoInefficiencies(List<String> directions) {
     if (directions.length < 4) return true;
-    if (directions[0] == directions[2] && directions[0] != directions[1])
+    if (directions[0] == directions[2] && directions[0] != directions[1]) {
       return false;
+    }
     return true;
   }
 
-  String layerRobots(int remainingLayers, List<List<String>> directions) {
-    if (directions.isEmpty) return "";
+  // Another cache that tracks start and end of the direction pad
+  // Returns the converted path possibilities
+  Map<String, List<List<String>>> dirCache = {};
+
+  // Recursive function that finds the length of the final command needed to
+  // command x number of robot layers
+  int layerRobots(int remainingLayers, String directions) {
+    if (directions.isEmpty) return 0;
     if (remainingLayers == 0) {
-      return directions.first.join("");
+      return directions.length;
     }
-    List<List<String>> newDirections = [];
-    for (final possibleDirections in directions) {
-      String dirKey = possibleDirections.join("");
-      if (cache.containsKey(dirKey)) {
-          newDirections.add(cache[dirKey]!.split("").toList());
-      } else {
-// print('possibleDirections: $possibleDirections');
-      if (possibleDirections.isEmpty) continue;
-      String start = 'A';
-      String joinedDirections = "";
-      for (int i = 0; i < possibleDirections.length; i++) {
-        String end = possibleDirections[i];
-        String key = '$start,$end,$remainingLayers';
-        if (cache.containsKey(key)) {
-          joinedDirections += cache[key]!;
-        } else {
-// Get initial inputs for possible keypad presses
-          List<List<Coordinate>> paths = getShortestPaths(start, end, dirPad);
+    // key is for the cache
+    String key = '$directions,$remainingLayers';
+    // Have we seen this one before at this depth?
+    if (cache.containsKey(key)) {
+      return cache[key]!;
+    } else {
+      // This is a newly encountered iteration, continue to get final length
+      // Split this instruction into pieces e.g. <<A^^A => [<<A, ^^A]
+      List<String> dividedDirections =
+          directions.split("A").map((e) => '${e}A').toList();
+      List<String> trimmedDirections =
+          dividedDirections.sublist(0, dividedDirections.length - 1);
+
+      // For each of these direction pieces
+      int totalLength = 0;
+      for (final direction in trimmedDirections) {
+        // Move through the direction, get paths, and repeat
+        List<String> splitDirection = direction.split("");
+        String start = 'A';
+        for (int i = 0; i < splitDirection.length; i++) {
+          String end = splitDirection[i];
+          String key = '$start,$end';
+          // Get initial inputs for possible keypad presses
           // Convert to direction strings
-          List<List<String>> directions = paths
+          List<List<String>> newDirections = dirCache.containsKey(key) ? dirCache[key]! : getShortestPaths(start, end, dirPad)
               .map(convertPathToInstructions)
               .where(hasNoInefficiencies)
               .toList();
-          // print('$start, $end, ${directions.join("")}');
 
-          // Use converter to get smallest amount of steps at x depth of robots
-          String convertedDirections =
-              layerRobots(remainingLayers - 1, directions);
-          cache[key] = convertedDirections;
-          // List<String> dividedDirections =
-          //     convertedDirections.split("A").map((e) => '${e}A').toList();
-          // List<String> trimmedDirections =
-          //     dividedDirections.sublist(0, dividedDirections.length - 1);
-          // print('trimmedDrections ${trimmedDirections}');
-          // print('convertedDirections: $convertedDirections');
-          joinedDirections += convertedDirections;
+          // Add to cache if not already there
+          if (!dirCache.containsKey(key)) dirCache[key] = newDirections;
+          // For each of these new directions possibilities, we have to check the outcome
+          // But we only care about the smallest
+          int minLength = newDirections
+              .map((d) => layerRobots(remainingLayers - 1, d.join("")))
+              .reduce(min);
+          totalLength += minLength;
+          start = direction[i];
         }
-
-        start = possibleDirections[i];
       }
-      newDirections.add(joinedDirections.split("").toList());
-      }
+      // Make sure we save this result before returning it
+      cache[key] = totalLength;
+      return totalLength;
     }
-    // All sublists have been converted, find the shortest one.
-    List<String> stringDirs = newDirections.map((e) => e.join("")).toList();
-    stringDirs.sort((a, b) => a.length - b.length);
-    return stringDirs.first;
   }
 
-  String makeDirectionsKey(List<List<String>> dirList, int layers) {
-    List<String> directions = dirList.map((e) => e.join("")).toList();
-    directions.sort();
-    return directions.join("") + layers.toString();
-  }
-
+  // Takes a coordinate path and converts to a list of string directions
   List<String> convertPathToInstructions(List<Coordinate> path) {
     List<String> instructions = [];
     for (int i = 1; i < path.length; i++) {
@@ -186,9 +175,17 @@ class Day21 extends Day {
     return instructions;
   }
 
-  // TODO: Cache this too
+  // An additional cache so we don't have to continue to calculate shortest paths
+  Map<String, List<List<Coordinate>>> pathCache = {};
+  // Gets the shortest path between two pad locations
+  // Basically a BFS
   List<List<Coordinate>> getShortestPaths(
       String from, String to, Map<String, Coordinate> pad) {
+    String padName = pad == numPad ? "num" : "dir";
+    String key = '$from,$to,$padName';
+    if (pathCache.containsKey(key)){
+      return pathCache[key]!;
+    }
     List<List<Coordinate>> paths = [];
     Coordinate start = pad[from]!;
     Coordinate end = pad[to]!;
@@ -214,9 +211,13 @@ class Day21 extends Day {
         min = path.length;
       }
     }
-    return paths.where((p) => p.length == min).toList();
+    List<List<Coordinate>> returnPaths = paths.where((p) => p.length == min).toList();
+    pathCache[key] = returnPaths;
+    return returnPaths;
   }
 
+  // Borrowing from earlier day.
+  // Gets neighbouring coordinates based on params
   List<Coordinate> getNextNeighbours(
       Coordinate start, int max, List<Coordinate> exclude,
       {int withinDistance = 1}) {
