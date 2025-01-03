948ea958c39c244284c586f1d31dbd8e7ffccdfb
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2025-01-02 16:43:58 -0800

    2024 Day 24 part 2

--- 2024/code/lib/days/Day24.dart
+++ 2024/code/lib/days/Day24.dart
@@ -13,6 +13,7 @@ class Day24 extends Day {
   Map<String, int> register = {};
   Map<String, Instruction> tree = {};
 
+  // Resets maps back to base input
   void reset() {
     register = {};
     tree = {};
@@ -31,13 +32,21 @@ class Day24 extends Day {
     }
   }
 
+  // Parses the maps to get the final values of all registers
   void run() {
     // Evaluate all the tree entries to build register values
     for (final entry in tree.entries) {
-      register[entry.key] = getRegisterValue(entry.key);
+      int value = getRegisterValue(entry.key, entry.key, true);
+      // If we get -1, there was an issue (found a cycle)
+      if (value == -1) {
+        throw Error();
+      }
+      register[entry.key] = value;
     }
   }
 
+  // Gets the binary value of all registers with a given prefix
+  // Only used for x, y, and z
   int getBinaryForLetter(String letter) {
     // Get all letter entries
     List<MapEntry<String, int>> entries =
@@ -53,15 +62,27 @@ class Day24 extends Day {
     return binary;
   }
 
+  // Just get the z outcome
   void part1() {
     run();
     int binary = getBinaryForLetter('z');
     print(binary);
   }
 
+  // There are 4 crossed wires that prevent the correct outcome
+  // Which wires are these?
+  // This is my initial solution. It's not good, but produces
+  // multiple possible answers, one of which is correct.
   void part2() {
+    // Doesn't work for test input
+    if (useTestData){
+      print("Not intended for test input");
+      exit(0);
+    }
     reset();
+
     // Don't want to include the last z entry in this swap list
+    // It's the one that would be the end of the flow
     List<int> zNums = tree.keys
         .where((k) => k.startsWith('z'))
         .map((s) => s.substring(1))
@@ -73,11 +94,13 @@ class Day24 extends Day {
         highest = num;
       }
     }
+    // Learned about these following conditions from online forums.
+    // It relates to the full adder configuration
     // If the output goes to a z, the operation must be XOR
     List<String> badZGates = tree.entries
         .where((entry) => entry.key.startsWith("z"))
         .where((entry) => tree[entry.key]!.op != Operation.XOR)
-        // .where((entry) => entry.key != 'z$highest')
+        .where((entry) => entry.key != 'z$highest')
         .map((e) => e.key)
         .toList();
 
@@ -92,11 +115,15 @@ class Day24 extends Day {
         .map((e) => e.key)
         .toList();
 
+    // There should be one more swap unrelated to the z registers
     List<String> remainingNodes = tree.keys
-        .where((k) => !badZGates.contains(k) && !badOtherGates.contains(k))
+        .where((k) =>
+            !badZGates.contains(k) &&
+            !badOtherGates.contains(k) &&
+            k != 'z$highest')
         .toList();
+
     // What are all the possible combinations of swaps for these known bad gates?
-    // Should be 9? (maybe 12) total
     List<({String a, String b})> combinations = [];
     for (String item1 in badZGates) {
       for (String item2 in badOtherGates) {
@@ -129,8 +156,8 @@ class Day24 extends Day {
         }
       }
     }
-    print(groups);
 
+    // Brute force through each possible combination
     for (final triplet in groups) {
       int swapIndexA = 0;
       while (swapIndexA < remainingNodes.length - 1) {
@@ -144,8 +171,13 @@ class Day24 extends Day {
           }
           // Swap two other nodes at random
           swapOutputs(remainingNodes[swapIndexA], remainingNodes[swapIndexB]);
-          run();
-
+          try {
+            run();
+          } catch (e) {
+            // loop detected. Abort and try next
+            continue;
+          }
+          // If x + y = z, then this is a possible solution
           if (thisSwapWorked()) {
             List<String> swappedNodes = [
               triplet[0].a,
@@ -159,56 +191,44 @@ class Day24 extends Day {
             ];
             swappedNodes.sort();
             print(swappedNodes.join(","));
-            exit(0);
           }
         }
         swapIndexA++;
       }
     }
-    print('Failure');
   }
 
   bool thisSwapWorked() {
-    return getBinaryForLetter('x') & getBinaryForLetter('y') ==
+    return getBinaryForLetter('x') + getBinaryForLetter('y') ==
         getBinaryForLetter('z');
   }
 
   void swapOutputs(String a, String b) {
-    // print('$a - $b');
     // Swap which gates make up this entry
     Instruction temp = tree[a]!;
     tree[a] = tree[b]!;
     tree[b] = temp;
-    // Any entries that reference this gate must be updated as well
-    for (final entry in tree.entries) {
-      if (entry.value.left == a) {
-        tree.update(entry.key, (e) => (left: b, op: e.op, right: e.right));
-      } else if (entry.value.right == a) {
-        tree.update(entry.key, (e) => (left: e.left, op: e.op, right: b));
-      } else if (entry.value.left == b) {
-        tree.update(entry.key, (e) => (left: a, op: e.op, right: e.right));
-      } else if (entry.value.right == b) {
-        tree.update(entry.key, (e) => (left: e.left, op: e.op, right: a));
-      }
-    }
   }
 
-  int getRegisterValue(String key) {
+  // Recursive function that gets a register's value or goes deeper to calculate it
+  int getRegisterValue(String key, String start, bool initial) {
+    // If we discover a full cycle, abort. It would be an endless loop otherwise.
+    if (key == start && !initial) return -1;
     if (register.containsKey(key)) {
       return register[key]!;
     }
     // Hasn't been calculated yet, get the sum of its parts
     Instruction instruction = tree[key]!;
     if (instruction.op == Operation.AND) {
-      return getRegisterValue(instruction.left) &
-          getRegisterValue(instruction.right);
+      return getRegisterValue(instruction.left, start, false) &
+          getRegisterValue(instruction.right, start, false);
     }
     if (instruction.op == Operation.OR) {
-      return getRegisterValue(instruction.left) |
-          getRegisterValue(instruction.right);
+      return getRegisterValue(instruction.left, start, false) |
+          getRegisterValue(instruction.right, start, false);
     }
-    return getRegisterValue(instruction.left) ^
-        getRegisterValue(instruction.right);
+    return getRegisterValue(instruction.left, start, false) ^
+        getRegisterValue(instruction.right, start, false);
   }
 
   Operation stringToOperation(String s) {
