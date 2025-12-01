6d7cf263397adc1845ead865c401815af541e50a
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2025-11-30 22:48:28 -0800

    2025 day01 solved

--- 2025/aoc2025/src/main/java/aoc/Main.java
+++ 2025/aoc2025/src/main/java/aoc/Main.java
@@ -3,7 +3,6 @@ package aoc;
 import aoc.utils.FileReader;
 import aoc.days.*;
 
-
 public class Main {
   public static void main(String[] args) {
     // Check arguments. Should be at least 1 argument: the day number
@@ -15,11 +14,12 @@ public class Main {
     boolean test = args.length > 1 && args[1].equals("test");
     switch (day) {
       case 1:
-      // print the working path
-      System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        // print the working path
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
         String filePath = test ? "input/01_test.txt" : "input/01.txt";
         FileReader fileReader = new FileReader(filePath);
         Day01.Part1(fileReader.lines);
+        Day01.Part2(fileReader.lines);
         break;
       // Add more cases for other days
       default:
--- 2025/aoc2025/src/main/java/aoc/days/Day01.java
+++ 2025/aoc2025/src/main/java/aoc/days/Day01.java
@@ -7,8 +7,67 @@ public class Day01 {
   public static void Part1(ArrayList<String> input) {
     // Implement Part 1 logic here
     System.out.println("Day 01, Part 1");
+    int currentValue = 50;
+    int zeroCount = 0;
     for (String line : input) {
-      System.out.println(line);
+      // System.out.println(line);
+      // Direction is first character
+      String direction = line.substring(0, 1);
+      int steps = Integer.parseInt(line.substring(1, line.length()));
+      // Turn dial left or right
+      int reducedMovement = steps % 100;
+      if (direction.equals("R")) {
+        currentValue += reducedMovement;
+        if (currentValue >= 100)
+          currentValue = currentValue % 100;
+      } else {
+        currentValue -= reducedMovement;
+        if (currentValue < 0) {
+          currentValue = 100 + currentValue;
+        }
+      }
+      // System.out.printf("Current value: %d\n", currentValue);
+      if (currentValue == 0)
+        zeroCount++;
     }
+    System.out.println(zeroCount);
+  }
+
+  public static void Part2(ArrayList<String> input) {
+    // Implement Part 1 logic here
+    System.out.println("Day 01, Part 1");
+    int currentValue = 50;
+    int zeroCount = 0;
+    for (String line : input) {
+      // System.out.println(line);
+      // Direction is first character
+      String direction = line.substring(0, 1);
+      int steps = Integer.parseInt(line.substring(1, line.length()));
+      // Turn dial left or right
+      int reducedMovement = steps % 100;
+      // System.out.printf("%s, %d, %d\n", line, currentValue, reducedMovement);
+      // Changed how we look for zero counts.
+      if (direction.equals("R")) {
+        // Right is easy. We just look at how many times the whole thing can be divided by the dial size.
+        zeroCount += (currentValue + steps) / 100;
+        currentValue = (currentValue + reducedMovement) % 100;
+      } else {
+        // Left sucks. 
+        // We don't want to count the case where we start at 0 then move left as a roll-over. 
+        // This only counts if we aren't starting at 0.
+        int firstHit = (currentValue == 0 ? 100 : currentValue);
+        if (steps >= firstHit) {
+          zeroCount += 1 + (steps - firstHit) / 100; // Note minimum of 1.
+        }
+        // Then update current value
+        currentValue -= reducedMovement;
+        if (currentValue < 0) {
+          currentValue = 100 + currentValue;
+        }
+      }
+      // System.out.printf("Zero count: %d\n", zeroCount);
+      // System.out.printf("Current value: %d\n", currentValue);
+    }
+    System.out.println(zeroCount);
   }
 }
