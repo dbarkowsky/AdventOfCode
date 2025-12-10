218b1fe4fe96a8f7776eb68d305da717d7a666b7
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2025-12-09 22:44:20 -0800

    2025 day 10 part 1 - works but slow

--- 2025/aoc2025/src/main/java/aoc/days/Day10.java
+++ 2025/aoc2025/src/main/java/aoc/days/Day10.java
@@ -1,7 +1,9 @@
 package aoc.days;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -47,14 +49,95 @@ public class Day10 {
 
   public void part1() {
     System.out.println("Day 10, Part 1");
+    ArrayList<Integer> fewestPresses = new ArrayList<>();
+    for (Machine m : machines) {
+      // Make a queue.
+      // Saves current state of lights, buttons pressed, etc.
+      Queue<State> stateQueue = new ArrayDeque<>();
+      // Add each button press as the first step.
+      // Each one is the start of a new timeline...
+      for (ArrayList<Integer> button : m.buttons) {
+        State s = new State(m.lights);
+        s.pressButton(button);
+        stateQueue.add(s);
+      }
 
-    System.out.println();
+      // Run Queue loop
+      // We'll run this until we break....
+      while (!stateQueue.isEmpty()) {
+        State current = stateQueue.poll();
+        // Did this state already turn off all the lights?
+        if (current.allLightsAreOff()) {
+          // We've done what we set out to do
+          // Add to list for total and move on
+          fewestPresses.add(current.steps);
+          break;
+        } else {
+          // Split the timeline again!
+          for (ArrayList<Integer> button : m.buttons) {
+            State s = new State(current);
+            s.pressButton(button);
+            stateQueue.add(s);
+          }
+        }
+      }
+    }
+    
+    // We've evaluated all machines. Get the total
+    long sum = 0;
+    for (Integer n : fewestPresses){
+      sum += n;
+    }
+
+    System.out.println(sum);
   }
 
   public void part2() {
     System.out.println("Day 10, Part 2");
 
     System.out.println();
+  }
+
+  private class State {
+    int steps = 0;
+    ArrayList<ArrayList<Integer>> buttonsPressed = new ArrayList<>();
+    ArrayList<String> lights;
+
+    public State(ArrayList<String> lights) {
+      this.lights = new ArrayList<>(lights);
+    }
+
+    public State(State original) {
+      this.steps = original.steps;
+      this.buttonsPressed = new ArrayList<>(original.buttonsPressed);
+      this.lights = new ArrayList<>(original.lights);
+    }
+
+    public void incrementStep() {
+      this.steps++;
+    }
+
+    public void pressButton(ArrayList<Integer> button) {
+      for (int i = 0; i < lights.size(); i++){
+        if (button.contains(i)){
+          if (lights.get(i).equals( "#")){
+            lights.set(i, ".");
+          } else {
+            lights.set(i, "#");
+          }
+        }
+      }
+      buttonsPressed.add(button);
+      this.incrementStep();
+    }
+
+    public boolean allLightsAreOff() {
+      for (String light : lights) {
+        if (light.equals("#"))
+          return false;
+      }
+      return true;
+    }
   }
 
   private class Machine {
