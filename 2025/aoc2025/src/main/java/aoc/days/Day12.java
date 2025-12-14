db1a6f17396c92c230d1803bdd9a86c36602fbe8
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2025-12-13 21:21:37 -0800

    update day 12 solution

--- 2025/aoc2025/src/main/java/aoc/days/Day12.java
+++ 2025/aoc2025/src/main/java/aoc/days/Day12.java
@@ -25,99 +25,20 @@ public class Day12 {
     }
   }
 
-  // Thoughts:
-  // need a way to score where a present goes
-  // Better score for having more sides against the boundary or against other
-  // presents
-  // or cost: higher cost for adjacent unfilled spaces...
+  // So, this didn't work for the test input, but it did for the real one
+  // I'm not happy about this, but I'll take it.
+  // Took 2.7 minutes to run though.
   public void part1() {
     System.out.println("Day 12, Part 1");
-    // For each space, we'll try to add one of each remaining presents from the list
-    // of requirements
-    // We can check each one in each direction
-    // If it fits, (no overlap), record space of center and score
-    // Fortunately, all presents are 3x3
+    // Nothing fancy here. Just see if everything fits and count it.
     int count = 0;
     for (Space s : spaces) {
       if (everythingFits(s))
         count++;
+      System.out.println(String.format("%d,%d", s.grid.width, s.grid.height));
+
     }
     System.out.println(count);
-  }
-
-  private boolean everythingFits(Space s) {
-    int[] requirementsCopy = s.requirements.clone();
-
-    while (requirementsRemain(requirementsCopy)) {
-      int maxScore = Integer.MIN_VALUE;
-      Point bestPoint = null;
-      BitGrid bestGrid = null;
-      int bestPresentIndex = -1;
-
-      // Build list of remaining presents (by index)
-      for (int i = 0; i < requirementsCopy.length; i++) {
-        if (requirementsCopy[i] <= 0)
-          continue;
-
-        Present p = presents.get(i);
-
-        ArrayList<BitGrid> possibleRotations = new ArrayList<>();
-        possibleRotations.add(p.shape);
-        possibleRotations.add(p.shape.rotate90CW());
-        possibleRotations.add(p.shape.rotate180());
-        possibleRotations.add(p.shape.rotate270CW());
-
-        // Try all rotations
-        for (BitGrid g : possibleRotations) {
-          int startRow = 0;//Math.max(1, s.grid.firstOccupiedRow() - 1);
-          Map<Point, Integer> result = s.grid.scoreAllPlacements(g, startRow);
-
-          for (Map.Entry<Point, Integer> e : result.entrySet()) {
-            if (e.getValue() > maxScore) {
-              maxScore = e.getValue();
-              bestPoint = e.getKey();
-              bestGrid = g;
-              bestPresentIndex = i;
-            }
-          }
-        }
-      }
-
-      // Nothing fits, fail early
-      if (bestPoint == null) {
-        return false;
-      }
-
-      // Apply best present to the space grid
-      applyGrid(s.grid, bestGrid, bestPoint);
-
-      // Consume one requirement
-      requirementsCopy[bestPresentIndex]--;
-    }
-
-    return true;
-  }
-
-  private void applyGrid(BitGrid space, BitGrid small, Point center) {
-    for (int idx = small.bits.nextSetBit(0); idx >= 0; idx = small.bits.nextSetBit(idx + 1)) {
-
-      int sr = idx / 3;
-      int sc = idx % 3;
-
-      int r = center.y + (sr - 1);
-      int c = center.x + (sc - 1);
-
-      space.set(r, c, true);
-    }
-  }
-
-  private boolean requirementsRemain(int[] r) {
-    for (int i : r) {
-      if (i != 0) {
-        return true;
-      }
-    }
-    return false;
   }
 
   public void part2() {
@@ -140,7 +61,97 @@ public class Day12 {
     System.out.println(count);
   }
 
+  private boolean everythingFits(Space s) {
+    // A copy so we can mutate
+    int[] requirementsCopy = s.requirements.clone();
+
+    // As long as there are presents to insert
+    while (requirementsRemain(requirementsCopy)) {
+      // System.out.println(requirementsCopy.toString());
+      // s.grid.print();
+      // We'll track the best-scoring option and insert that one
+      int maxScore = Integer.MIN_VALUE;
+      Point bestPoint = null;
+      BitGrid bestGrid = null;
+      int bestPresentIndex = -1;
+      int startRow = 1;
+
+      // Build list of remaining presents (by index) for one pass
+      for (int i = 0; i < requirementsCopy.length; i++) {
+        if (requirementsCopy[i] <= 0)
+          continue;
+
+        Present p = presents.get(i);
+        // We need to check every possible rotation for this...
+        ArrayList<BitGrid> possibleRotations = new ArrayList<>();
+        possibleRotations.add(p.shape);
+        possibleRotations.add(p.shape.rotate90CW());
+        possibleRotations.add(p.shape.rotate180());
+        possibleRotations.add(p.shape.rotate270CW());
+
+        // Try all rotations
+        for (BitGrid g : possibleRotations) {
+          Map<Point, Integer> result = s.grid.scoreAllPlacements(g, startRow);
+
+          // If it has the
+          for (Map.Entry<Point, Integer> e : result.entrySet()) {
+            if (e.getValue() > maxScore) {
+              maxScore = e.getValue();
+              bestPoint = e.getKey();
+              bestGrid = g;
+              bestPresentIndex = i;
+            }
+          }
+        }
+        // A small attempt to get this to run faster. Start at a row we know isn't
+        // filled yet.
+        startRow = Math.max(1, s.grid.lastOccupiedRow() - 2);
+      }
+
+      // Nothing fits, fail early
+      if (bestPoint == null) {
+        return false;
+      }
+
+      // Apply best present to the space grid
+      applyGrid(s.grid, bestGrid, bestPoint);
+      // Update the last occupied row.
+      s.grid.lastRow = s.grid.lastOccupiedRow();
+
+      // Consume one requirement
+      requirementsCopy[bestPresentIndex]--;
+    }
+
+    return true;
+  }
+
+  // Puts the present into the Space at appropriate location
+  private void applyGrid(BitGrid space, BitGrid small, Point center) {
+    for (int i = small.bits.nextSetBit(0); i >= 0; i = small.bits.nextSetBit(i + 1)) {
+
+      int sourceRow = i / 3;
+      int sourceColumn = i % 3;
+
+      int row = center.y + (sourceRow - 1);
+      int column = center.x + (sourceColumn - 1);
+
+      space.set(row, column, true);
+    }
+  }
+
+  // Makes sure there are still presents to insert
+  private boolean requirementsRemain(int[] r) {
+    for (int i : r) {
+      if (i != 0) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // A class to represent each present.
   private class Present {
+    // Been meaning to use BitGrid for a while. Finally a chance.
     BitGrid shape = new BitGrid(3, 3);
 
     public Present(List<String> presentLines) {
@@ -157,8 +168,10 @@ public class Day12 {
     }
   }
 
+  // A class to represent each box Space
   private class Space {
     BitGrid grid;
+    // the list of presents to insert
     int[] requirements = new int[6];
 
     public Space(String spaceLine) {
@@ -174,11 +187,14 @@ public class Day12 {
     }
   }
 
+  // I needed a BitSet but in two dimensions. Could have probably done it
+  // with some clever indexing, but oh well.
+  // This grew way bigger than I expected it to.
   public class BitGrid {
     private final int width;
     private final int height;
     private final BitSet bits;
-    int lastRow = lastOccupiedRow();
+    private int lastRow = lastOccupiedRow();
 
     public BitGrid(int width, int height) {
       this.width = width;
@@ -186,6 +202,7 @@ public class Day12 {
       this.bits = new BitSet(width * height);
     }
 
+    // Some simple getters and setters
     private int index(int row, int col) {
       return row * width + col;
     }
@@ -202,15 +219,17 @@ public class Day12 {
       bits.flip(index(row, col));
     }
 
+    // Some methods to get all rotations of the grid
+    // Used for rotating presents
     public BitGrid rotate90CW() {
       BitGrid out = new BitGrid(height, width); // swapped dimensions
 
-      for (int r = 0; r < height; r++) {
-        for (int c = 0; c < width; c++) {
-          if (get(r, c)) {
-            int nr = c;
-            int nc = height - 1 - r;
-            out.set(nr, nc, true);
+      for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+          if (get(row, col)) {
+            int newRow = col;
+            int newCol = height - 1 - row;
+            out.set(newRow, newCol, true);
           }
         }
       }
@@ -221,12 +240,12 @@ public class Day12 {
     public BitGrid rotate180() {
       BitGrid out = new BitGrid(width, height);
 
-      for (int r = 0; r < height; r++) {
-        for (int c = 0; c < width; c++) {
-          if (get(r, c)) {
-            int nr = height - 1 - r;
-            int nc = width - 1 - c;
-            out.set(nr, nc, true);
+      for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+          if (get(row, col)) {
+            int newRow = height - 1 - row;
+            int newCol = width - 1 - col;
+            out.set(newRow, newCol, true);
           }
         }
       }
@@ -237,12 +256,12 @@ public class Day12 {
     public BitGrid rotate270CW() {
       BitGrid out = new BitGrid(height, width); // swapped dimensions
 
-      for (int r = 0; r < height; r++) {
-        for (int c = 0; c < width; c++) {
-          if (get(r, c)) {
-            int nr = width - 1 - c;
-            int nc = r;
-            out.set(nr, nc, true);
+      for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+          if (get(row, col)) {
+            int newRow = width - 1 - col;
+            int newCol = row;
+            out.set(newRow, newCol, true);
           }
         }
       }
@@ -250,6 +269,7 @@ public class Day12 {
       return out;
     }
 
+    // Because I need debugging help
     public void print() {
       for (int r = 0; r < height; r++) {
         StringBuilder sb = new StringBuilder(width);
@@ -261,17 +281,19 @@ public class Day12 {
       System.out.println();
     }
 
-    public BitGrid get3x3(int r, int c) {
+    // Gets a 3x3 subset of the grid
+    // Used to extract a portion of Shapes
+    public BitGrid get3x3(int row, int col) {
       BitGrid out = new BitGrid(3, 3);
 
-      for (int dr = -1; dr <= 1; dr++) {
-        for (int dc = -1; dc <= 1; dc++) {
-          int sr = r + dr;
-          int sc = c + dc;
+      for (int rowDiff = -1; rowDiff <= 1; rowDiff++) {
+        for (int colDiff = -1; colDiff <= 1; colDiff++) {
+          int sr = row + rowDiff;
+          int sc = col + colDiff;
 
           // If in bounds, copy its value
           if (sr >= 0 && sr < height && sc >= 0 && sc < width) {
-            out.set(dr + 1, dc + 1, get(sr, sc));
+            out.set(rowDiff + 1, colDiff + 1, get(sr, sc));
           }
           // Else leave it false.
         }
@@ -280,6 +302,8 @@ public class Day12 {
       return out;
     }
 
+    // Checks if two grids can merge without collisions
+    // aka no 1s in the same places
     // Only designed with checking two 3x3 grids
     public boolean couldMerge(BitGrid other) {
       BitSet a = this.bits;
@@ -296,9 +320,10 @@ public class Day12 {
       return bits.isEmpty();
     }
 
+    // Returns a score based on whether surrounding blocks have 1 values
     public int scorePlacement(BitGrid small, int centerR, int centerC) {
-      int[] DR = { -1, 1, 0, 0 };
-      int[] DC = { 0, 0, -1, 1 };
+      int[] rowDifference = { -1, 1, 0, 0 };
+      int[] columnDifference = { 0, 0, -1, 1 };
       BitGrid window = this.get3x3(centerR, centerC);
 
       // Must be merge-compatible
@@ -309,21 +334,21 @@ public class Day12 {
       int score = 0;
 
       // Iterate over bits set in the small grid
-      for (int idx = small.bits.nextSetBit(0); idx >= 0; idx = small.bits.nextSetBit(idx + 1)) {
+      for (int i = small.bits.nextSetBit(0); i >= 0; i = small.bits.nextSetBit(i + 1)) {
 
-        int sr = idx / 3; // 0..2
-        int sc = idx % 3; // 0..2
+        int sr = i / 3; // 0..2
+        int sc = i % 3; // 0..2
 
         int lr = centerR + (sr - 1);
         int lc = centerC + (sc - 1);
 
         // Check adjacency in the large grid
         for (int d = 0; d < 4; d++) {
-          int ar = lr + DR[d];
-          int ac = lc + DC[d];
+          int adjacentRow = lr + rowDifference[d];
+          int adjacentCol = lc + columnDifference[d];
 
-          if (ar >= 0 && ar < height && ac >= 0 && ac < width) {
-            if (this.get(ar, ac)) {
+          if (adjacentRow >= 0 && adjacentRow < height && adjacentCol >= 0 && adjacentCol < width) {
+            if (this.get(adjacentRow, adjacentCol)) {
               score++;
             }
           }
@@ -333,6 +358,9 @@ public class Day12 {
       return score;
     }
 
+    // Looks at all places this present could go.
+    // Returns a map with centre points and its score
+    // 3x3 only can be inserted.
     public Map<Point, Integer> scoreAllPlacements(BitGrid small, int startRow) {
       if (small.width != 3 || small.height != 3)
         throw new IllegalArgumentException("small grid must be 3x3");
@@ -340,27 +368,26 @@ public class Day12 {
       Map<Point, Integer> result = new HashMap<>();
 
       // Clamp startRow so a 3x3 window fits
-      int r0 = Math.max(1, startRow);
+      int rowStart = Math.max(1, startRow);
       int rMax = height - 2;
 
-      int lastRow = lastOccupiedRow();
       int maxSmallBits = small.bits.cardinality();
 
-      for (int r = r0; r <= rMax; r++) {
+      for (int row = rowStart; row <= rMax; row++) {
 
         // Early exit: everything below is empty
-        if (r - 1 > lastRow)
+        if (row - 2 > lastRow)
           break;
 
-        for (int c = 1; c < width - 1; c++) {
+        for (int col = 1; col < width - 1; col++) {
 
           // Optional fast skip for saturated areas
-          if (count3x3(r, c) > maxSmallBits)
+          if (count3x3(row, col) > maxSmallBits)
             continue;
 
-          int score = scorePlacement(small, r, c);
+          int score = scorePlacement(small, row, col);
           if (score >= 0) {
-            result.put(new Point(c, r), score);
+            result.put(new Point(col, row), score);
           }
         }
       }
@@ -368,6 +395,8 @@ public class Day12 {
       return result;
     }
 
+    // What's the last occupied row?
+    // The idea here was to skip the already full rows
     public int lastOccupiedRow() {
       if (bits == null) {
         return -1;
@@ -379,57 +408,22 @@ public class Day12 {
       return lastBit / width;
     }
 
-    public int firstOccupiedRow() {
-      for (int r = 0; r < height; r++) {
-        for (int c = 0; c < width; c++) {
-          if (get(r, c)) { // get(r, c) checks the bit at row r, col c
-            return r;
-          }
-        }
-      }
-      return height; // no bits set
-    }
-
+    // Just gets all "on" bits in this 3x3 area
     public int count3x3(int centerR, int centerC) {
       int count = 0;
 
       for (int dr = -1; dr <= 1; dr++) {
         for (int dc = -1; dc <= 1; dc++) {
-          int r = centerR + dr;
-          int c = centerC + dc;
+          int row = centerR + dr;
+          int col = centerC + dc;
 
-          if (r >= 0 && r < height && c >= 0 && c < width) {
-            if (get(r, c))
+          if (row >= 0 && row < height && col >= 0 && col < width) {
+            if (get(row, col))
               count++;
           }
         }
       }
       return count;
-    }
-
-    public int rowBitCount(int row) {
-      int count = 0;
-      for (int c = 0; c < width; c++) {
-        if (get(row, c))
-          count++;
-      }
-      return count;
-    }
-
-    public int firstViableRow(BitGrid small) {
-      int limit = small.bits.cardinality();
-
-      for (int r = 1; r < height - 1; r++) {
-        int bandCount = rowBitCount(r - 1)
-            + rowBitCount(r)
-            + rowBitCount(r + 1);
-
-        if (bandCount <= limit) {
-          return r;
-        }
-      }
-
-      return height; // no viable placement
     }
   }
 }
